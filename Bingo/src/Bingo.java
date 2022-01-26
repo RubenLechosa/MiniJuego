879a21d220ae53969fc6f83dd20606cefa7d85e8
Rulexloko13 <rubenlechosa@gmail.com>
2022-01-26 19:56:06 +0100

    nuevo

--- Bingo/src/Bingo.java
+++ Bingo/src/Bingo.java
@@ -5,8 +5,6 @@ public class Bingo {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int elegir = 0;
 		int num = 0;
 
 		System.out.println(" /$$$$$$$ /$$$$$$/$$   /$$ /$$$$$$  /$$$$$$       \r\n"
@@ -18,22 +16,21 @@ public class Bingo {
 				+ "| $$$$$$$//$$$$$| $$ \\  $|  $$$$$$|  $$$$$$/      \r\n"
 				+ "|_______/|______|__/  \\__/\\______/ \\______/   \n    ");
 
-		System.out.println("Elige un modo de juego: \n [1]Jugador vs Jugador \n [2]Jugador vs IA");
-		elegir = sc.nextInt();
-
-		while (elegir < 1 || elegir > 2) {
-			System.out.println("Error: Elige un modo de juego: [1]Jugador vs Jugador [2]Jugador vs IA");
-		}
-
-		if (elegir == 1) {
+		int elegir = 0;
+		do {
+			System.out.println("1- IA o 2- JUGADORES");
+			elegir = sc.nextInt();
+		} while (elegir != 1 && elegir != 2);
+		
+		if(elegir == 1){
+			String carton[][][] = new String[2][3][5];
+			rellenar(carton);
+			bingo(carton, true);
+		}else{
 			num = cuantos();
 			String carton[][][] = new String[num][3][5];
 			rellenar(carton);
-
-			bingo(carton);
-
-		} else {
-			System.out.println("Esto es 2");
+			bingo(carton, false);
 		}
 	}
 
@@ -41,14 +38,14 @@ public class Bingo {
 		Scanner teclado = new Scanner(System.in);
 
 		int numero = 0;
+		do {
+			try {
+				System.out.println("Inserta el numero de Jugadores: (max 6)");
+				numero = Integer.parseInt(teclado.next());
+			} catch (Exception e) {
 
-		System.out.println("Has elegido jugador vs jugador, inserta el numero de jugadores:(max 6)");
-		numero = teclado.nextInt();
-
-		while (numero < 1 || numero > 6) {
-			System.out.println("Error: inserta el numero de jugadores: (max 6)");
-			numero = teclado.nextInt();
-		}
+			}
+		} while (numero < 1 || numero > 6);
 
 		return numero;
 
@@ -90,13 +87,15 @@ public class Bingo {
 		}
 	}
 
-	public static void bingo(String mapa[][][]) {
+	public static void bingo(String mapa[][][], boolean ia) {
 		Random aleatorio = new Random();
 
 		int temp = 0;
 		int vector[] = new int[90];
 		boolean repetido = true;
 		boolean fin = false;
+		boolean comprobar = false;
+		boolean encontrada = false;
 
 		System.out.println();
 		mostrar(mapa);
@@ -112,11 +111,16 @@ public class Bingo {
 						repetido = true;
 					}
 				}
-
 				if (repetido == false) {
 					vector[i] = temp;
-
 					for (int x = 0; x < mapa.length; x++) {
+						comprobar = linea(mapa[x]);
+
+						if (comprobar == true && encontrada == false) {
+							System.out.println("Linea del jugador "+(x+1));
+							encontrada = true;
+						}
+
 						if (lleno(mapa[x]) == true) {
 							fin = true;
 							System.out.println("BINGO, el jugador " + (x + 1) + " ha ganado");
@@ -125,22 +129,22 @@ public class Bingo {
 					}
 					if (fin == false) {
 						try {
-							Thread.sleep(1*200);
+							Thread.sleep(1 * 200);
 						} catch (Exception e) {
 							System.out.println(e);
 						}
 						System.out.print(vector[i] + " ");
 
 						for (int z = 0; z < mapa.length; z++) {
-							
+
 							for (int x = 0; x < mapa[z].length; x++) {
 								for (int k = 0; k < mapa[z][x].length; k++) {
 									if (Integer.parseInt(mapa[z][x][k]) == temp) {
 										mapa[z][x][k] = "-1";
 										System.out.println("\n");
 										mostrar(mapa);
-										System.out.println(
-												"\nSe ha encontrado el numero " + temp + ", seguimos para BINGO \n");
+										System.out.println("\nSe ha encontrado el numero " + temp
+												+ " en el carton del jugador" + x + ", seguimos para BINGO \n");
 										try {
 											Thread.sleep(500);
 										} catch (Exception e) {
@@ -176,6 +180,25 @@ public class Bingo {
 		return lleno;
 	}
 
+	public static boolean linea(String[][] carton) {
+
+		int contador = 0;
+
+		for (int z = 0; z < carton.length; z++) {
+			contador = 0;
+			for (int j = 0; j < carton[z].length; j++) {
+				if (carton[z][j].equals("-1")) {
+					contador++;
+				}
+			}
+			if (contador == 5) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+
 	public static boolean comprobarRepString(String vector[][][], String temporal) {
 		for (int i = 0; i < vector.length; i++) {
 			for (int z = 0; z < vector[i].length; z++) {
@@ -189,5 +212,4 @@ public class Bingo {
 		}
 		return false;
 	}
-
 }
